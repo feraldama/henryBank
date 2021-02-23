c9dd834eb206f97f60314d7c428def8b6398b877
chulas21 <prattofrancisko@gmail.com>
2021-02-23 12:15:14 -0300

    exluded .expo

--- client/res/colors.js
+++
@@ -1,7 +0,0 @@
-export default colors = {
-	primary: '#324B50',
-	secondary: '#95B0B6',
-	accent: '#77C5D5',
-	white: '#fff',
-	black: '#000',
-};
--- client/screens/index.js
+++
@@ -1,5 +0,0 @@
-//las screens, o componentes, las exportamos como const
-//ej: const Home = () => {}
-//y acá exportamos todas las screen, para poder accederlas
-//con destructuring
-//ej: export * from './Home'
