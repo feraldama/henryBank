061b970a5161bbac5867170f9011bec687f4e6bf
chulas21 <prattofrancisko@gmail.com>
2021-02-23 12:12:36 -0300

    resources added

--- client/App.js
+++ client/App.js
@@ -1,28 +1,32 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Provider as ReduxProvider } from "react-redux";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import colors from './res/colors.js';
+import { StyleSheet, View, Image } from 'react-native';
+import { Provider as ReduxProvider } from 'react-redux';
 
-import generateStore from "./store";
+import generateStore from './store';
 
 export default function App() {
-  const store = generateStore();
+	const store = generateStore();
 
-  return (
-    <ReduxProvider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </ReduxProvider>
-  );
+	return (
+		<ReduxProvider store={store}>
+			<View style={styles.container}>
+				<Image source={require('./assets/logo2.png')} style={styles.image} />
+			</View>
+		</ReduxProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: colors.primary,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	image: {
+		width: 200,
+		height: 200,
+	},
 });
