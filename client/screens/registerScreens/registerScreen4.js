aa0eff57a1788d133d925621edc2f16af9c76495
chulas21 <prattofrancisko@gmail.com>
2021-02-24 12:10:56 -0300

    Fixed path to res directory

--- client/screens/registerScreens/registerScreen4.js
+++ client/screens/registerScreens/registerScreen4.js
@@ -7,7 +7,7 @@ import {
 } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import { Button, TextInput } from "react-native-paper";
-import colors from "../res/colors";
+import colors from "../../res/colors";
 
 function registerScreen4() {
   const [state, setState] = useState({
