9b5ad2e6bc7123573f26448312d4438d8d0765f2
chulas21 <prattofrancisko@gmail.com>
2021-02-24 13:16:41 -0300

    Fixed store import error

--- client/App.js
+++ client/App.js
@@ -3,16 +3,12 @@ import colors from "./res/colors.js";
 import { StyleSheet } from "react-native";
 import { Provider as ReduxProvider } from "react-redux";
 import HomeStack from "./navigation/homeStack.js";
-import generateStore from "./store";
+import store from "./redux/store.js";
 
 export default function App() {
-  const store = generateStore();
-
-
   return (
     <ReduxProvider store={store}>
       <HomeStack />
     </ReduxProvider>
   );
 }
-
