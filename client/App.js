768ccaec6bc762fca77cac945d6a5b2bdea08d88
Wilmer21 <hachikoestick87@gmail.com>
2021-02-24 11:32:38 -0500

    vista login
    metodo de store

--- client/App.js
+++ client/App.js
@@ -3,11 +3,9 @@ import colors from "./res/colors.js";
 import { StyleSheet } from "react-native";
 import { Provider as ReduxProvider } from "react-redux";
 import HomeStack from "./navigation/homeStack.js";
-import generateStore from "./store";
+import store from "./redux/store";
 
 export default function App() {
-  const store = generateStore();
-
 
   return (
     <ReduxProvider store={store}>
