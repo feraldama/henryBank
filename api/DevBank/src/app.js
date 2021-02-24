e2894179a7748322c7d278833ebf2aa2b2974741
dafevilo <danstev50@gmail.com>
2021-02-24 17:03:23 -0500

    Post User

--- api/DevBank/src/app.js
+++ api/DevBank/src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
-const routes = require('./routes/index')
+const routes = require('./routes/index');
+const bodyParser = require("body-parser");
 
 const server = express();
 
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
 server.use(morgan('dev'))
 server.use('/', routes);
+
+
 
 module.exports = server;
