6f165e7dde3779cabf4ad48552c92da9bee1d6a6
adriandante <zed.iaf@gmail.com>
2021-02-24 14:29:31 -0300

    log in, index colors

--- client/App.js
+++ client/App.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { Provider as ReduxProvider } from "react-redux";
 import HomeStack from "./navigation/homeStack.js";
-<<<<<<< HEAD
-import store from "./redux/store";
-=======
 import store from "./redux/store.js";
->>>>>>> 1f1d2d43aafd28f900bc4f979e964f41e019fc58
 
 export default function App() {
   return (
