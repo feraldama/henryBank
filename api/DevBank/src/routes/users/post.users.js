2b66fdb67d9092b891e7e39c9a09ed8c8950958d
SantiagoNHQ <santi_hidalgo@hotmail.com>
2021-02-24 19:23:26 -0300

    password encriptada con bcrypt

--- api/DevBank/src/routes/users/post.users.js
+++ api/DevBank/src/routes/users/post.users.js
@@ -1,12 +1,18 @@
 const server = require('express').Router();
 const userController = require('../../controllers/users.controller');
 const {User} = require('../../database/db')
+var bcrypt = require("bcryptjs");
 
 
 
 server.post('/', (req, res) => {
-    const user = req.body
-    const pass = req.body.password
+    var user = req.body
+    var password = req.body.password
+
+    var salt = bcrypt.genSaltSync(10);
+    user.password = bcrypt.hashSync(password, salt);
+
+    user.password = req.body.password
 
     userController.createUser(user)
         .then((value) => {
