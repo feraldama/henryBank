3d79790dc864fd781f3321e71d8d5bec7aac2f4f
dafevilo <danstev50@gmail.com>
2021-02-25 15:32:27 -0500

    Link working well

--- api/DevBank/src/routes/users/post.users.js
+++ api/DevBank/src/routes/users/post.users.js
@@ -3,7 +3,7 @@ const userController = require('../../controllers/users.controller');
 const nodemailerController = require('../../controllers/nodemailer.controller');
 const nodemailer = require('nodemailer');
 var bcrypt = require("bcryptjs");
-const {User} = require('../../database/db')
+const { User } = require('../../database/db')
 
 
 
@@ -21,28 +21,30 @@ server.post('/', (req, res) => {
                 return res.status(400).json({ msg: 'User already exist' })
             }
 
+            const url = "www.google.com"
+
             const data = {
                 from: 'DevBank <devbank2021@gmail>',
                 to: `${value.email}`,
                 subject: "Welcome to DevBank",
-                html:  `
+                html: `
                         <h2> Nice to meet you </h2>
                         <br>
                         <br>
                         <br>
-                        <p> Almost ready, first click <a href='localhost:8080/users/${value.id}'>here</a> and finish your check Out</p> 
+                        <p> Almost ready, first click <a href="http://localhost:8080/users/${value.id}">here</a> and finish your check Out</p> 
                         <br>
                         <br>
                         <br>
                         <p><strong>Thank you so much</strong></p>
-                       `
+                    `
 
             }
-            
-            // return nodemailerController.sendEmail(data)
-        })  
+
+            return nodemailerController.sendEmail(data)
+        })
         .then(() => {
-            res.status(200).json({msg: 'Check your email'})
+            res.status(200).json({ msg: 'Check your email' })
         })
         .catch((err) => {
             res.status(400).json(err)
@@ -51,23 +53,23 @@ server.post('/', (req, res) => {
 
 server.post('/:userId', (req, res) => {
     const info = req.body
-    const {userId} = req.params
+    const { userId } = req.params
 
     userController.getOneUser(userId)
         .then((user) => {
-            if(!user){
-                return res.status(400).json({msg:'User does not exist'})
+            if (!user) {
+                return res.status(400).json({ msg: 'User does not exist' })
             }
             return userController.updateInfo(user, info)
         })
         .then((user) => {
-            var cvu = 10000001233- userId + 500
-            var data= {userId, acconutNumber:1 , balance: 0, currency: "PESOS", cvu, type:"CAJA DE AHORRO"}
+            var cvu = 10000001233 - userId + 500
+            var data = { userId, acconutNumber: 1, balance: 0, currency: "PESOS", cvu, type: "CAJA DE AHORRO" }
             return userController.createdAccount(data)
         })
         .then((user) => {
-            var cvu =  10000001233- userId + 1000
-            var data= {userId, acconutNumber:2 , balance: 0, currency: "USD", cvu, type:"CAJA DE AHORRO"}
+            var cvu = 10000001233 - userId + 1000
+            var data = { userId, acconutNumber: 2, balance: 0, currency: "USD", cvu, type: "CAJA DE AHORRO" }
             return userController.createdAccount(data)
         })
         .then((response) => {
