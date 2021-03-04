293908990615c17a8874f2f069e61a60996d2a93
Carlos Salamanca <67124765+carlos2365@users.noreply.github.com>
2021-03-04 18:27:43 -0500

    Add files via upload

--- api/DevBank/src/routes/index.js
+++ api/DevBank/src/routes/index.js
@@ -6,14 +6,22 @@ const postUsers = require('./users/post.users');
 const getUsers = require('./users/get.users');
 const postAuth = require('./auth/post.auth');
 const transfer = require("./transfer/tranfer")
+const contact = require ("./contact/contact")
+const account = require("./account/account")
+
 
 
 // tranfer
 router.use('/users/transfer', transfer);
 // auth login
 router.use('/users/auth',postAuth);
+//Contact route
+router.use('/users/contact', contact);
+// Account rute
+router.use('/account', account);
 // use Users
 router.use('/users', postUsers);
 router.use('/users', getUsers);
+
 
 module.exports = router;
