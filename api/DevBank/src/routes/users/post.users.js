4a2a6b07dd06510f267757a418e255c601a3bb4d
Fernando Aldama <fer.ald87@gmail.com>
2021-03-05 14:00:26 -0300

    mensajes y validaciones

--- api/DevBank/src/routes/users/post.users.js
+++ api/DevBank/src/routes/users/post.users.js
@@ -7,17 +7,17 @@ var bcrypt = require("bcryptjs");
 const { User, Account } = require("../../database/db");
 
 server.post("/", (req, res) => {
-  var user = req.body;
-  var password = req.body.password;
-  // var salt = bcrypt.genSaltSync(10);
-  // user.password = bcrypt.hashSync(password, salt);
+  let user = req.body;
   var salt = bcrypt.genSaltSync(10);
-  var hash = bcrypt.hashSync(password, salt);
+  var hash = bcrypt.hashSync(user.password, salt);
   user.password = hash;
+
+  let created;
 
   userController
     .createUser(user)
     .then((value) => {
+      created = value;
       if (!value) {
         return res.status(400).json({ msg: "User already exist" });
       }
@@ -26,20 +26,28 @@ server.post("/", (req, res) => {
         from: "DevBank <devbank2021@gmail>",
         to: `${value.email}`,
         subject: "Welcome to DevBank",
-        html: `
-                        <h2> Nice to meet you </h2>
-                        <br>
-                        <br>
-                        <br>
-                        <p> Almost ready, first click <a href='http://localhost:8000/${value.id}'>here</a> and finish your check Out</p> 
-                        <br>
-                        <br>
-                        <br>
-                        <p><strong>Thank you so much</strong></p>
-                    `,
+        html: `<h1> Nice to meet you </h1>
+        <br>
+        <br>
+        <h3> Check Out Completed </h3>
+        <br>
+        <p> ${value.name} Open your app and enjoy it</p> 
+        <br>
+        <br>
+        <br>
+        <p><strong>Thank you so much</strong></p>`,
       };
 
       return nodemailerController.sendEmail(data);
+    })
+    .then(() => {
+      return accountController.getAccount(created.id);
+    })
+    .then((acc) => {
+      if (acc) {
+        return res.status(200).json({ msg: "Accounts were created before" });
+      }
+      return accountController.createdAccount(created.id);
     })
     .then(() => {
       res.status(200).json({ msg: "Check your email" });
@@ -48,6 +56,49 @@ server.post("/", (req, res) => {
       res.status(400).json(err);
     });
 });
+
+// server.post("/", (req, res) => {
+//   var user = req.body;
+//   var password = req.body.password;
+//   // var salt = bcrypt.genSaltSync(10);
+//   // user.password = bcrypt.hashSync(password, salt);
+//   var salt = bcrypt.genSaltSync(10);
+//   var hash = bcrypt.hashSync(password, salt);
+//   user.password = hash;
+
+//   userController
+//     .createUser(user)
+//     .then((value) => {
+//       if (!value) {
+//         return res.status(400).json({ msg: "User already exist" });
+//       }
+
+//       const data = {
+//         from: "DevBank <devbank2021@gmail>",
+//         to: `${value.email}`,
+//         subject: "Welcome to DevBank",
+//         html: `
+//                         <h2> Nice to meet you </h2>
+//                         <br>
+//                         <br>
+//                         <br>
+//                         <p> Almost ready, first click <a href='http://localhost:8000/${value.id}'>here</a> and finish your check Out</p>
+//                         <br>
+//                         <br>
+//                         <br>
+//                         <p><strong>Thank you so much</strong></p>
+//                     `,
+//       };
+
+//       return nodemailerController.sendEmail(data);
+//     })
+//     .then(() => {
+//       res.status(200).json({ msg: "Check your email" });
+//     })
+//     .catch((err) => {
+//       res.status(400).json(err);
+//     });
+// });
 
 server.post("/:userId", (req, res) => {
   const info = req.body;
