e2733eddc46575cb869eb88828dd6136ae08357b
AdrianDante <68869790+AdrianDante@users.noreply.github.com>
2021-03-11 17:43:21 -0300

    Add files via upload

--- client/redux/actions_types.js
+++ client/redux/actions_types.js
@@ -7,3 +7,4 @@ export const ACCOUNT_LOGIN = "ACCOUNT_LOGIN";
 export const RECARGAR_DINERO = "RECARGAR_DINERO";
 export const EMPTY_REDUX = "EMPTY_REDUX";
 export const TRANSFER = "TRANSFER";
+export const UPDATE_USER_PIC = "UPDATE_USER_PIC";
